b52daebe6bf970ab3ac4ebd1e3e942ed9960581e
Ulviyyava <101628207+Ulviyyava@users.noreply.github.com>
2022-07-05 04:26:58 +0400

    Add files via upload

---
+++ FR1.1/app.js
@@ -0,0 +1,27 @@
+let hour=new Date().getHours();
+
+console.log(hour);
+
+if(hour===6-12){
+
+    alert('sabahiniz xeyir');
+
+}
+else {
+
+    alert('sagolun');
+}
+
+
+
+
+const customers = [1,2,3];
+const surname = ["Zahidova", "Bayramova","Aliyeva"];
+const name = ["Sofiya", "Ulker","Melissa"];
+
+const myChildren = array1.concat(array2, array3); 
+
+
+
+
+
---
+++ FRONT1.2/app.js
@@ -0,0 +1,6 @@
+
+const array1 = [1,2,3];
+const array2 = ["Zahidova", "Bayramova","Aliyeva"];
+const array3 = ["Sofiya", "Ulker","Melissa"];
+
+const myCustomer = array1.concat(array2, array3); 
